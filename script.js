981585c76b89753153d360786a01b6a3d278856a
katherineclare9-design <katherineclare9@gmail.com>
2026-08-17 22:02:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,8 @@ const introNextBtn = document.getElementById("introNextBtn");
 
 introNextBtn.addEventListener("click", () => {
 
-    // Hide intro
     introScreen.classList.remove("active");
 
-    // Show main app
     mainApp.classList.add("active");
 
 });
